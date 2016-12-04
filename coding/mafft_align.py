23a2c002a972f77160468113f0ad8120eb64847d
espinha <ruiespinharibeiro@ua.pt>
2016-12-04 06:00:02 +0000

    Added mafft alignment. Muscle not working correctly

--- coding/mafft_align.py
+++ coding/mafft_align.py
@@ -9,11 +9,18 @@ from Bio import SeqIO
 import time
 
 mafft = r"/usr/local/bin/mafft"
-mafft_cline = MafftCommandline('mafft', input=r"source_sequences/short_version.fna", output=r"source_sequences/mafft.fna")
+mafft_cline = MafftCommandline('mafft', input=r"source_sequences/short_version.fna")
 
 t0 = time.time()
+
 stdout, stderr = mafft_cline()
-print time.time() - t0
+
+print (time.time() - t0)/60 #time in minutes
+
+dst_name = 'source_sequences/mafft.fasta'
+with open(dst_name, "w") as handle:
+    handle.write(stdout)
+align = AlignIO.read(dst_name, "fasta")
 
 """tree = Phylo.read('short_version.dnd', 'newick')
 print tree
---
+++ coding/muscle_align.py
@@ -0,0 +1,29 @@
+from Bio import AlignIO
+#from Bio.Align.Applications import MuscleCommandline
+from Bio.Align.Applications import MuscleCommandline
+from Bio import Phylo
+
+from StringIO import StringIO
+from Bio import SeqIO
+
+import time
+import subprocess
+import sys
+
+muscle = r"/usr/local/bin/muscle3.8.31_i86linux64"
+
+muscle_cline = MuscleCommandline(muscle,input=r"source_sequences/short_version.fna", out=r"source_sequences/muscle.fna")
+
+t0 = time.time()
+#child = subprocess.Popen(str(muscle_cline), stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, shell=(sys.platform!="win32"))
+
+stdout, stderr = muscle_cline()
+align = AlignIO.read(stdout, "fasta")
+print align
+#stdout, stderr = muscle_cline()
+print time.time() - t0
+
+"""tree = Phylo.read('short_version.dnd', 'newick')
+print tree
+
+Phylo.draw_ascii(tree)"""
